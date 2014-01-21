6643402a5fbf327ce2687296d5d1d8ed4312dac2
Alec Geatches <throwpi@me.com>
2014-01-21 12:49:23 -0800

    Added branch checkout and tests

--- git.clj
+++ git.clj
@@ -5,18 +5,17 @@
                        :commit nil}]
     {:name name
      :head master-branch
+     :branches [master-branch]
      :last-commit-id -1}))
 
 (defn commit [repo message]
-  (let
-    [commit-id (inc (:last-commit-id repo))
-     this-commit {:id commit-id
-                  :message message
-                  :parent (:commit (:head repo))}]
-    {:name (:name repo)
-     :last-commit-id commit-id
-     :head {:name (:name (:head repo))
-            :commit this-commit}}))
+  (let [commit-id (inc (:last-commit-id repo))
+        this-commit {:id commit-id
+                     :message message
+                     :parent (:commit (:head repo))}
+        head (assoc (:head repo) :commit this-commit)]
+    (assoc repo :last-commit-id commit-id
+                :head head)))
 
 (defn inner-log [a-commit]
   (let [parent (:parent a-commit)]
@@ -26,8 +25,20 @@
 
 (defn log [repo] (inner-log (:commit (:head repo))))
 
-;; Poor-man's testing
+(defn checkout [repo branch-name]
+  (let
+    [index-of-branch (.indexOf (map :name (repo :branches)) branch-name)
+     branch-exists (not= index-of-branch -1)]
+    (if branch-exists
+      (assoc repo :head (get (:branches repo) index-of-branch))
+      (let [new-branch {:name branch-name
+                        :commit (:commit (:head repo))}]
+        (assoc repo :head new-branch
+                    :branches (conj (:branches repo) new-branch))))))
 
+;;;; Poor-man's testing
+
+;; Commit tests
 (def repo (git "test"))
 (def repo (commit repo "Inital commit"))
 (def repo (commit repo "Change 1"))
@@ -35,3 +46,17 @@
 (= (count (log repo)) 2) ;; Should have two commits
 (= (:id (first (log repo))) 1) ;; Commit 1 should be first
 (= (:id (second (log repo))) 0) ;; Commit 0 should be second
+
+;; Checkout tests
+(def repo (git "test"))
+(def repo (commit repo "Initial commit"))
+(= (:name (:head repo)) "master") ;; Should be on master branch
+
+(def repo (checkout repo "testing"))
+(= (:name (:head repo)) "testing") ;; Should be on testing branch
+
+(def repo (checkout repo "master"))
+(= (:name (:head repo)) "master") ;; Should be on master branch
+
+(def repo (checkout repo "testing"))
+(= (:name (:head repo)) "testing") ;; Should be on testing branch again.
