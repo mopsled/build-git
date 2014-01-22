8abb32618190f065a6b5432dc3823058dfa70469
Alec Geatches <throwpi@me.com>
2014-01-22 12:41:13 -0800

    Finished branch checkout and commit and added tests

--- git.clj
+++ git.clj
@@ -8,14 +8,23 @@
      :branches [master-branch]
      :last-commit-id -1}))
 
+(defn add-commit-to-head-branch [repo commit]
+  (let
+    [index-of-branch (.indexOf (map :name (:branches repo)) (:name (:head repo)))
+     old-branch (get (:branches repo) index-of-branch)
+     new-branch (assoc old-branch :commit commit)
+     modified-branches (assoc (:branches repo) index-of-branch new-branch)]
+    (assoc repo :branches modified-branches)))
+
 (defn commit [repo message]
   (let [commit-id (inc (:last-commit-id repo))
         this-commit {:id commit-id
                      :message message
                      :parent (:commit (:head repo))}
-        head (assoc (:head repo) :commit this-commit)]
-    (assoc repo :last-commit-id commit-id
-                :head head)))
+        head (assoc (:head repo) :commit this-commit)
+        repo-with-modified-head (assoc repo :last-commit-id commit-id
+                                            :head head)]
+    (add-commit-to-head-branch repo-with-modified-head this-commit)))
 
 (defn inner-log [a-commit]
   (let [parent (:parent a-commit)]
@@ -60,3 +69,27 @@
 
 (def repo (checkout repo "testing"))
 (= (:name (:head repo)) "testing") ;; Should be on testing branch again.
+
+;; Branch tests
+(def repo (git "test"))
+(def repo (commit repo "Initial commit"))
+(def repo (commit repo "Change 1"))
+
+(defn log-to-id-history [log]
+  (clojure.string/join "-" (map :id log)))
+
+(= (log-to-id-history (log repo)) "1-0") ;; Should show 2 commits
+
+(def repo (checkout repo "testing"))
+(def repo (commit repo "Change 2"))
+(= (log-to-id-history (log repo)) "2-1-0") ;; Should show 3 commits
+
+(def repo (checkout repo "master"))
+
+(log-to-id-history (log repo))
+repo
+
+(= (log-to-id-history (log repo)) "1-0") ;; Should show 2 commits
+
+(def repo (commit repo "Change 3"))
+(= (log-to-id-history (log repo)) "3-1-0") ;; Should show 3 commits
